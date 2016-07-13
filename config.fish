a8f99916a3c923d867a3fb562770fd74a125ce8c
Denis Aoki <dnsaoki@Deniss-MacBook-Pro.local>
2016-07-13 14:05:31 -0300

    adicionando config do fish shell

---
+++ config.fish
@@ -0,0 +1,11 @@
+# Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG "/Users/dnsaoki/.config/omf"
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
+eval (python -m virtualfish compat_aliases auto_activation)
