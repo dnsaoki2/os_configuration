f401e8e2954cdcc4f0845dacf7b43f94acb8caec
Denis Aoki <dnsaoki@gmail.com>
2017-06-14 10:51:04 -0300

    Backup 14/06/2017

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,25 +8,47 @@ set -q XDG_DATA_HOME
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
-
-# Golang configuration
+# eval (python -m virtualfish compat_aliases auto_activation)
 eval (export GOPATH=$HOME/go)
 eval (export GO15VENDOREXPERIMENT=1)
+# eval (export PATH=$PATH:$GOPATH/bin)
 set PATH $PATH $GOPATH/bin
+# set PATH $PATH "/Users/denis.aoki/workspace/personal/cilk/bin"
+eval (export LC_ALL=en_US.UTF-8)
+eval (export LANG=en_US.UTF-8)
+# set C_INCLUDE_PATH $C_INCLUDE_PATH "/Users/denis.aoki/workspace/personal/cilk/include"
+# set CPLUS_INCLUDE_PATH $CPLUS_INCLUDE_PATH "/Users/denis.aoki/workspace/personal/cilk/include"
+# set LIBRARY_PATH $LIBRARY_PATH "/Users/denis.aoki/workspace/personal/cilk/lib"
+# set DYLD_LIBRARY_PATH $DYLD_LIBRARY_PATH "/Users/denis.aoki/workspace/personal/cilk/lib"
 
-# Alias dir
-alias personal "cd $HOME/workspace/personal"
-
-# Alias git
+########## alias ##########
+set -gx WORKSPACE "$HOME/workspace"
+## diretorios
+alias work "cd $WORKSPACE/work"
+alias personal "cd $WORKSPACE/personal"
+alias pub "cd $WORKSPACE/work/gitlab/publicacao-core"
+alias estrutura "cd $WORKSPACE/work/gitlab/nova_estrutura"
+alias api "cd $WORKSPACE/work/gitlab/apis"
+alias backstage "cd $WORKSPACE/work/gitlab/backstage"
+## git
 alias gs "git status"
+alias ga "git add ."
 alias gp "git pull"
 alias gc "git checkout"
 alias gl "git log"
+alias gd "git diff"
 
-# Alias editors
-alias pycharm "open -a /Applications/Pycharm.app"
+alias pycharm 'open -a /Applications/PyCharm.app'
 
-# Start pyenv
+# Highlight current day
+alias today 'cal | grep --color -EC6 "\b"(date +%e | sed "s/ //g")'
+rvm default
+
+alias sub "subl ."
+
 set -gx PYENV_ROOT $HOME/.pyenv
 set -gx PATH $PYENV_ROOT/shims $PYENV_ROOT/bin $PATH
-pyenv rehash
+# pyenv rehash
+# set -q PYENV_ROOT; or set -l PYENV_ROOT $HOME/.pyenv
+
+# set PATH $PYENV_ROOT/shims $PYENV_ROOT/bin $PATH
---
+++ theme_fish/fish_right_prompt.fish
@@ -0,0 +1,63 @@
+
+
+function fish_right_prompt -d "Right side prompt message"
+
+    # A dark grey
+    set --local dark_grey 555
+
+    set_color $dark_grey
+
+	show_pyenv_name
+    show_git_info
+    echo -en (date +%H:%M:%S)
+
+    set_color normal
+end
+
+function show_pyenv_name -d "pyenv virtualenv"
+	set --local GLOBAL_NAME (pyenv global)
+	set --local VERSION_NAME (pyenv version-name)
+	if [ ! -z $VERSION_NAME  ]
+		if [ $GLOBAL_NAME != $VERSION_NAME  ]
+			echo -n "["$VERSION_NAME"] "
+		end
+	end
+end
+
+
+function show_virtualenv_name -d "Display the name of the current virtualenv"
+
+    if set -q VIRTUAL_ENV
+        echo -en "["(basename "$VIRTUAL_ENV")"] "
+    end
+end
+
+
+function show_git_info -d "Show git repository information"
+
+    set --local LIMBO /dev/null
+    set --local git_status (git status --porcelain 2> $LIMBO)
+    set --local dirty ""
+
+    [ $status -eq 128 ]; and return  # Not a repository? Nothing to do
+
+    # If there is modifications, set repository dirty to '*'
+    if not [ -z (echo "$git_status" | grep -e '^ M') ]
+        set dirty "*"
+    end
+
+    # If there is new or deleted files, add  '+' to dirty
+    if not [ -z (echo "$git_status" | grep -e '^[MDA]') ]
+        set dirty "$dirty+"
+    end
+
+    # If there is stashed modifications on repository, add '^' to dirty
+    if not [ -z (git stash list) ]
+        set dirty "$dirty^"
+    end
+
+    # Prints git repository status
+    echo -en "("
+    echo -en (git_branch_name)$dirty
+    echo -en ") "
+end
